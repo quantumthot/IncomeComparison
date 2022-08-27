819fac7dfc99be47041e0ae0ef0953b31e5ba4b3
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-08-27 15:11:12 -0700

    Add files via upload
    update math

--- IncomeComparison/Program.cs
+++ IncomeComparison/Program.cs
@@ -13,21 +13,31 @@ namespace IncomeComparison
 
             Console.WriteLine("Person 1: Hours worked per week?");
             int yourHours1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Person 1, your annual salary is:");
-            Console.WriteLine(yourRate1 * yourHours1 * 52);
-
+         
             Console.WriteLine("Person 2: What is your hourly rate?");
             int yourRate2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Person 2: Hours worked per week?");
             int yourHours2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Person 2, your annual salary is:");
-            Console.WriteLine(yourRate2 * yourHours2 * 52);
+            Console.WriteLine("Person 1, your annual salary is:");
+            int a = yourRate1;
+            int b = yourHours1;
+            int c = (a * b * 52);
+            Console.WriteLine(c);
 
-            Console.WriteLine("Does Person 1 make more money than Person 2? True or False?");
-            string trueFalse = Console.ReadLine();// will store a string.
+            Console.WriteLine("Person 2, your annual salary is:");
+            int d = yourRate2;
+            int e = yourHours2;
+            int f = (d * e * 52);
+            Console.WriteLine(f);
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(c > f);
+
+
+
+
         }
     }
 }
